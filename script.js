b4b5d262cdbf8fc2f6da236a3c90f246588c3eff
Taras Kvasnytsa <kwas@MacBook-Pro-Taras.local>
2021-08-12 23:26:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+//EXAMPLE
+const name = 'Taras'
+const age = 31
+const header = document.querySelector('#header')
+const header2 = document.getElementById('header')
+const navLinks = document.querySelectorAll('.nav__link')
+console.log(navLinks);
+
+function sayHello() {
+	console.log('Hi');
+}
+sayHello()
+function plusString() {
+	let msg = 'age=' + 31
+	console.log(msg);
+}
+plusString()
+function sum(b) {
+	let a = 25
+	let sum = 25+25*2+a
+	let sumMinus = sum - 50
+	let sumMinusPlusGiveNumberInFunction = sum - 50 + b
+	console.log(sum);
+	console.log(sumMinus);
+	console.log(sumMinusPlusGiveNumberInFunction);
+	return sumMinusPlusGiveNumberInFunction
+}
+let result = sum(1000)
+console.log(result);
+
+
+let user = {
+	age: 20,
+	name: 'Taras',
+}
+
+
+
+// how create object
+// read https://javascriptissexy.com/oop-in-javascript-what-you-need-to-know/
+//git clone https://github.com/KwaSTaraS/calsMeatPorridge.git
+
+// 		learn
+//git commit
+//git push
+//git branch
+		//need know
+//git cherry-pick
+//git reset
+//git merge
+//git status
+//git add
